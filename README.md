b3e729aa9819496f6414f01876d3fc6728ff049d
mammix2 <matt@ffptechnologies.co.uk>
2014-06-09 18:46:33 +0100

    BurnerCoin

---
+++ README.md
@@ -0,0 +1,4 @@
+
+BurnerCoin 
+
+x13 Proof of Work + Proof of Stake hybrid coin with Bonus PoW blocks!
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BurnerCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BurnerCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
